64f7094a835e9a5de61c87afd1582e94edee525b
slhad <sylvainrivierre@gmail.com>
2013-12-08 21:38:44 +0100

    Scrollable with autoscroll

--- Source/FSCR/Main.Designer.cs
+++ Source/FSCR/Main.Designer.cs
@@ -31,13 +31,15 @@
             this.components = new System.ComponentModel.Container();
             this.timerCheck = new System.Windows.Forms.Timer(this.components);
             this.bIsEnabled = new System.Windows.Forms.Button();
-            this.tbLog = new System.Windows.Forms.TextBox();
             this.cbRun = new System.Windows.Forms.CheckBox();
             this.tbServer = new System.Windows.Forms.TextBox();
             this.nUDPort = new System.Windows.Forms.NumericUpDown();
             this.lPort = new System.Windows.Forms.Label();
             this.lServer = new System.Windows.Forms.Label();
+            this.pScrollable = new System.Windows.Forms.Panel();
+            this.tbLog = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.nUDPort)).BeginInit();
+            this.pScrollable.SuspendLayout();
             this.SuspendLayout();
             // 
             // timerCheck
@@ -54,20 +56,6 @@
             this.bIsEnabled.Text = "Off";
             this.bIsEnabled.UseVisualStyleBackColor = true;
             this.bIsEnabled.Click += new System.EventHandler(this.bIsEnabled_Click);
-            // 
-            // tbLog
-            // 
-            this.tbLog.AcceptsReturn = true;
-            this.tbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbLog.BackColor = System.Drawing.SystemColors.Window;
-            this.tbLog.Location = new System.Drawing.Point(12, 41);
-            this.tbLog.Multiline = true;
-            this.tbLog.Name = "tbLog";
-            this.tbLog.ReadOnly = true;
-            this.tbLog.Size = new System.Drawing.Size(610, 217);
-            this.tbLog.TabIndex = 1;
             // 
             // cbRun
             // 
@@ -129,21 +117,47 @@
             this.lServer.TabIndex = 6;
             this.lServer.Text = "Server";
             // 
+            // pScrollable
+            // 
+            this.pScrollable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pScrollable.AutoScroll = true;
+            this.pScrollable.Controls.Add(this.tbLog);
+            this.pScrollable.Location = new System.Drawing.Point(12, 42);
+            this.pScrollable.Name = "pScrollable";
+            this.pScrollable.Size = new System.Drawing.Size(610, 225);
+            this.pScrollable.TabIndex = 7;
+            // 
+            // tbLog
+            // 
+            this.tbLog.AcceptsReturn = true;
+            this.tbLog.BackColor = System.Drawing.SystemColors.Window;
+            this.tbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbLog.Location = new System.Drawing.Point(0, 0);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.Size = new System.Drawing.Size(610, 225);
+            this.tbLog.TabIndex = 2;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(634, 270);
+            this.Controls.Add(this.pScrollable);
             this.Controls.Add(this.lServer);
             this.Controls.Add(this.lPort);
             this.Controls.Add(this.nUDPort);
             this.Controls.Add(this.tbServer);
             this.Controls.Add(this.cbRun);
-            this.Controls.Add(this.tbLog);
             this.Controls.Add(this.bIsEnabled);
             this.Name = "Main";
             this.Text = "Full Screen Check and Run";
             ((System.ComponentModel.ISupportInitialize)(this.nUDPort)).EndInit();
+            this.pScrollable.ResumeLayout(false);
+            this.pScrollable.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,12 +167,13 @@
 
         private System.Windows.Forms.Timer timerCheck;
         private System.Windows.Forms.Button bIsEnabled;
-        private System.Windows.Forms.TextBox tbLog;
         private System.Windows.Forms.CheckBox cbRun;
         private System.Windows.Forms.TextBox tbServer;
         private System.Windows.Forms.NumericUpDown nUDPort;
         private System.Windows.Forms.Label lPort;
         private System.Windows.Forms.Label lServer;
+        private System.Windows.Forms.Panel pScrollable;
+        private System.Windows.Forms.TextBox tbLog;
     }
 }
 
